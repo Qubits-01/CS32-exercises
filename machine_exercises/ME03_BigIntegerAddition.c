3b39e048e0dd9a525e39146a3685c8e26844a9ed
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-03-18 08:16:32 +0800

    Initialize Doubly linked-list with head implementation

--- machine_exercises/ME03_BigIntegerAddition.c
+++ machine_exercises/ME03_BigIntegerAddition.c
@@ -2,8 +2,17 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
+typedef struct BigInt {
+    int data;
+    struct BigInt* llink;
+    struct BigInt* rlink;
+} BigInt;
+
 int main() {
-    
+    BigInt bigint;
+    bigint.data = 12;
+
+    printf("%d", bigint.data);
 
     return 0;
 }

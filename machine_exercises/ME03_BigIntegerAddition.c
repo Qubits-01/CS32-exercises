bb465bc1e5cd3c1dc212761cdc0c00deb6b893eb
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-03-22 18:56:14 +0800

    Add some parts of multiprecision long int addition

--- machine_exercises/ME03_BigIntegerAddition.c
+++ machine_exercises/ME03_BigIntegerAddition.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdbool.h>
 
 typedef struct BigInt {
     int data;
@@ -8,19 +7,138 @@ typedef struct BigInt {
     struct BigInt* rlink;
 } BigInt;
 
-BigInt* char_to_bigint(char *digits, int len) {
-    // 
+
+// Converts an array of characters (where each character is a digit).
+BigInt * char_to_bigint(char *digits, int len) {
+    // zeroInteger
+    BigInt *longInt = NULL;
+    BigInt *tao = (BigInt *) malloc(sizeof(BigInt));
+    
+    tao -> data = 0;
+    tao -> llink = tao;
+    tao -> rlink = tao;
+    longInt = tao;
+    // *zeroInteger
+
+    int nTerms = 0;
+    int sign = 1;
+
+    for (int i = 0; i < len; i++) {
+        int term = digits[i] - '0';  // Converts [char] digit into [int].
+
+        // insertAtTail
+        tao = (BigInt *) malloc(sizeof(BigInt));
+        tao -> data = term;
+        (longInt -> llink) -> rlink = tao;
+        tao -> rlink = longInt;
+        tao -> llink = longInt -> llink;
+        longInt -> llink = tao;
+        // *insertAtTail
+
+        nTerms += 1;
+    }
+    
+    longInt -> data = sign * nTerms;
+    return longInt;
 }
 
-BigInt* add(BigInt **a_ptr, BigInt **b_prt) {
-    //
+// Takes as input pointers to list head pointers for the two
+// [BigInt]s to add and outputs a pointer to a new [BigInt].
+BigInt * add(BigInt **a_ptr, BigInt **b_ptr) {
+    BigInt *a = *a_ptr;
+    BigInt *b = *b_ptr;
+
+    // zeroInteger
+    BigInt *sum = NULL;
+    BigInt *tao = (BigInt *) malloc(sizeof(BigInt));
+    
+    tao -> data = 0;
+    tao -> llink = tao;
+    tao -> rlink = tao;
+    sum = tao;
+    // *zeroInteger
+
+    // [k] is number of terms in [sum].
+    // [c] is carry.
+    int k = 0;
+    int c = 0;
+    BigInt *alpha = a -> llink;
+    BigInt *beta = b -> llink;
+
+    // for (int i = 0; i < 3; i++) {
+    //     printf("%d\n", alpha -> data);
+    //     alpha = alpha -> rlink;
+    // }
+
+    int t = 0;
+    while (1) {
+        if ((alpha != a) && (beta != b)) {
+            t = (alpha -> data) + (beta -> data) + c;
+            alpha = alpha -> llink;
+            beta = beta -> llink;
+        }
+
+        if ((alpha != a) && (beta == b)) {
+            t = (alpha -> data) + c;
+            alpha = alpha -> llink;
+        }
+
+        if ((alpha == a) && (beta != b)) {
+            t = (beta -> data) + c;
+            beta = beta -> llink;
+        }
+
+        if ((alpha == a) && (beta == b)) {
+            if (c != 0) {
+                // insertAtHead
+                BigInt *tao1 = (BigInt *) malloc(sizeof(BigInt));
+                tao1 -> data = c;
+                tao1 -> rlink = sum -> rlink;
+                tao1 -> llink = sum;
+                (sum -> rlink) -> llink = tao1;
+                sum -> rlink = tao1;
+                // *insertAtHead
+
+                k += 1;
+            } else {
+                // deleteLeadingZeroes
+                // *deleteLeadingZeroes
+            }
+
+            sum -> data = k;
+            break;
+        }
+
+        int term = t % 10;
+        // insertAtHead
+        BigInt *tao2 = (BigInt *) malloc(sizeof(BigInt));
+        tao2 -> data = term;
+        tao2 -> rlink = sum -> rlink;
+        tao2 -> llink = sum;
+        (sum -> rlink) -> llink = tao2;
+        sum -> rlink = tao2;
+        // *insertAtHead
+
+        c = t / 10;
+        k += 1;
+    }
+
+    return sum;
 }
+
 
 int main() {
-    BigInt bigint;
-    bigint.data = 12;
+    char n1[10] = "12212";
+    char n2[10] = "34531";
 
-    printf("%d", bigint.data);
+    BigInt *a = char_to_bigint(n1, 5);
+    BigInt *b = char_to_bigint(n2, 5);
+    BigInt *sum = add(&a, &b);
+
+    for (int i = 0; i < 20; i++) {
+        sum = sum -> rlink;
+        printf("%d ", sum -> data);
+    }
 
     return 0;
 }

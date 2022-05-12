39e94736646f132273eeb2862e8d058fa8e9328c
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-05-12 08:24:37 +0800

    Refactor code and Add ME05_Quicksort (Perfect Score)

--- machine_exercises/ME05_QuickSort.c
+++ machine_exercises/ME05_QuickSort.c
@@ -80,65 +80,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define arrSize 8
-#define prime 13
+#define arrSize 1000  // 8
+#define prime 524287  // 13
 #define newLine printf("\n")
 
-void displayArr(int myArr[]) {
-    for (int i = 0; i < arrSize; i++) {
-        printf("%d ", myArr[i]);
-    }
-    newLine;
-}
-
-void displayArrWithRange(int myArr[], int left, int right) {
-    for (;left < right; left++) {
-        printf("%d ", myArr[left]);
-    }
-    newLine;
-}
-
-int partition(int A[], int p, int r) {
-    int x = A[r];
-    int i = p - 1;
-
-    for (int j = p; j <= r - 1; j++) {
-        if (A[j] <= x) {
-            i++;
-            int temp = A[i];
-            A[i] = A[j];
-            A[j] = temp;
-        }
-    }
-
-    int temp = A[i + 1];
-    A[i + 1] = A[r];
-    A[r] = temp;
-
-    displayArrWithRange(A, p, r + 1);
-    
-    return i + 1;
-}
-
-void quicksort(int A[], int p, int r) {
-    if (p < r) {
-        int q = partition(A, p, r);
-        quicksort(A, p, q - 1);
-        quicksort(A, q + 1, r);
-    }
-}
-
-
-void shuffleArr(int newArr[], int oldArr[]) {
-    int newIndices[arrSize];
-    for (int i = 0; i < arrSize; i++) {
-        newIndices[i] = ((i + 1) * prime) % arrSize;
-    }
-
-    for (int i = 0; i < arrSize; i++) {
-        newArr[i] = oldArr[newIndices[i]];
-    }
-}
+void quicksort(int[], int, int);
+int partition(int[], int, int);
+void shuffleArr(int[], int[]);
+void displayArr(int[]);
+void displayArrWithRange(int[], int, int);
 
 int main() {
     int noOfTestcases;
@@ -164,4 +114,50 @@ int main() {
     }
 
     return 0;
+}
+
+void quicksort(int A[], int p, int r) {
+    if (p < r) {
+        int q = partition(A, p, r);
+        quicksort(A, p, q - 1);
+        quicksort(A, q + 1, r);
+    }
+}
+
+int partition(int A[], int p, int r) {
+    int x = A[r];
+    int i = p - 1;
+
+    for (int j = p; j <= r - 1; j++) {
+        if (A[j] <= x) {
+            i++;
+            int temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    }
+
+    int temp = A[i + 1];
+    A[i + 1] = A[r];
+    A[r] = temp;
+
+    displayArrWithRange(A, p, r + 1);
+    
+    return i + 1;
+}
+
+void shuffleArr(int newArr[], int oldArr[]) {
+    int newIndices[arrSize];
+    for (int i = 0; i < arrSize; i++) newIndices[i] = ((i + 1) * prime) % arrSize;
+    for (int i = 0; i < arrSize; i++) newArr[i] = oldArr[newIndices[i]];
+}
+
+void displayArr(int myArr[]) {
+    for (int i = 0; i < arrSize; i++) printf("%d ", myArr[i]);
+    newLine;
+}
+
+void displayArrWithRange(int myArr[], int left, int right) {
+    for (;left < right; left++) printf("%d ", myArr[left]);
+    newLine;
 }

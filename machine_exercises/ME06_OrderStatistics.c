faa1efb8bcc2c147edaab88bb41d7d2fa800291e
Marc Peejay Viernes <mpvviernes@gmail.com>
2022-05-29 10:31:51 +0800

    Remove comments

--- machine_exercises/ME06_OrderStatistics.c
+++ machine_exercises/ME06_OrderStatistics.c
@@ -4,22 +4,6 @@
 
 #define newLine printf("\n");
 
-// void insertion_sort(int A[], int from, int to) {
-//     for(int i = from; i < to; i++) {
-//         for(int j = i + 1; j > from && A[j] < A[j - 1]; j--) {
-//             int temp = A[j - 1];
-//             A[j - 1] = A[j];
-//             A[j] = temp;
-//         }
-//     }
-// }
-
-void displayArray(int A[], int from, int to) {
-    for (int i = from; i < to; i++) {
-        printf("%d ", A[i]);
-    }
-}
-
 int ceilDiv(int numerator, int denominator) {
     int floorQuotient = numerator / denominator;
     int ceilQuotient;
@@ -47,20 +31,11 @@ void insertion_sort(int A[], int from, int to) {
   }
 }
 
-
-
 int quick_select(int A[], int p, int r, int k);
 
 int median_of_medians(int A[], int p, int r){
 	// Write your code here
     int noOfGroups =  ceilDiv((r - p) + 1, 5);
-    
-    // printf("    p: %d, r: %d\n", p, r);
-    // printf("    A: ");
-    // displayArray(A, 0, r + 1);
-    // newLine;
-    // printf("    noOfGroups: %d\n", noOfGroups);
-    
     int medians[noOfGroups];
     int mediansIndex = 0;
 
@@ -68,60 +43,25 @@ int median_of_medians(int A[], int p, int r){
     int tempGroupIndex = 0;
     
     for (int i = p; i <= r; i++) {
-        // printf("%d, ", A[i]);
-        // newLine;
-          
-        // printf("      tempGroupIndex: %d, A[i]: %d\n", tempGroupIndex, A[i]);
-        
         tempGroup[tempGroupIndex] = A[i];
         tempGroupIndex++;
-        
-        // printf("    unsortedTempGroup: ");
-        // displayArray(tempGroup, 0, tempGroupIndex);
-        // newLine;
 
         if ((tempGroupIndex == 5) || (i == r)) {
-            // printf("tempGroupIndex: %d, i: %d\n", tempGroupIndex, i);
-            
             insertion_sort(tempGroup, 0, tempGroupIndex);
 
-            // printf("index: %d, value: %d\n", ((tempGroupIndex + 1) / 2) - 1, tempGroup[(tempGroupIndex + 1) / 2] - 1);
             medians[mediansIndex] = tempGroup[((tempGroupIndex + 1) / 2) - 1];
             mediansIndex++;
-            
-            // printf("    sortedTempGroup: ");
-            // displayArray(tempGroup, 0, tempGroupIndex);
-            // newLine;
 
             tempGroupIndex = 0;
         }
     }
 
-    // printf("    unsortedMoM: ");
-    // displayArray(medians, 0, noOfGroups);
-    // newLine;
-
     return quick_select(medians, 0, noOfGroups - 1, (noOfGroups + 1) / 2);
-    // insertion_sort(medians, 0, noOfGroups);
-    // int medianOfMedians = medians[((noOfGroups + 1) / 2) - 1];
-    
-    // printf("    sortedMoM: ");
-    // displayArray(medians, 0, noOfGroups);
-    // newLine;
-    
-    // printf("    medianOfMedians: %d\n", medianOfMedians);
-    
-    // return medianOfMedians;
 }
 
 int median_partition(int A[], int p, int r){
 	// Write your code here
     int median = median_of_medians(A, p, r);
-    
-    // printf("  median: %d, p: %d, r: %d\n", median, p, r);
-    // printf("  A: ");
-    // displayArray(A, 0, r);
-    // newLine;
 
     int ind = -1;
     for (int i = p; i <= r; i++) {
@@ -130,19 +70,11 @@ int median_partition(int A[], int p, int r){
             break;
         }
     }
-    
-    // printf("  ind: %d\n", ind);
-
-    // if (ind == -1) {
-    //     printf("Error!!!\n");
-    //     exit(1);
-    // }
 
     int temp;
     temp = A[ind];
     A[ind] = A[r];
     A[r] = temp;
-    // A[ind] = A[r];
 
     int x = A[r];
     int i = p - 1;
@@ -153,23 +85,19 @@ int median_partition(int A[], int p, int r){
             temp = A[i];
             A[i] = A[j];
             A[j] = temp;
-            // A[i] = A[j];
         }
     }
 
     temp = A[i + 1];
     A[i + 1] = A[r];
     A[r] = temp;
-    // A[i + 1] = A[r];
 
     return i + 1;
 }
 
 int quick_select(int A[], int p, int r, int rank){
 	// Write your code here
-    if (p == r) {
-        return A[p];
-    }
+    if (p == r) return A[p];
 
     int q = median_partition(A, p, r);
     int k = q - p + 1;
@@ -192,25 +120,10 @@ int main(){
 		int arr[M];
 
 		//read the elements of the input array
-		for(int i=0; i<M; i++){
-			if (scanf("%d", &arr[i])) {};
-		}
+		for(int i=0; i<M; i++) if (scanf("%d", &arr[i])) {};
         
-        // printf("input: ");
-        // displayArray(arr, 0, M);
-        // newLine;
-        
-        // printf("sort: ");
-        // insertion_sort(arr, 0, M);
-        // displayArray(arr, 0, M);
-        // newLine;
-
-        // median_of_medians(arr, 0, M - 1);
-
 		int median_index = ((M + 1) / 2);
 		printf("Median: %d\n", quick_select(arr, 0, M - 1, median_index));
-		
-		// newLine;
 	}
 	
 	return 0;
